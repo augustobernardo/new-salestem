57c8cad76abe3f64c3531f86a42f6a42d2adba0b
augustobernardo <gutooliveira027@gmail.com>
2023-04-13 21:53:19 -0300

    Criando o Projeto

---
+++ src/App.vue
@@ -0,0 +1,319 @@
+<script setup lang="ts">
+// import Sidebar from "./components/Sidebar.vue";
+// import Content from "./components/Content.vue";
+import { onMounted } from "vue";
+
+const allSideMenu = () => {
+  const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+  allSideMenu.forEach(item => {
+    const li = item.parentElement as HTMLElement;
+
+    item.addEventListener('click', function () {
+      allSideMenu.forEach(i => {
+        const liParent = i.parentElement as HTMLElement;
+        liParent.classList.remove('active');
+      })
+      li.classList.add('active');
+    })
+  });
+}
+
+
+
+// create a function menubar and pass the menuBar and sideBar as parameter
+const menuBar = () => {
+  const menuBar = document.querySelector('#content nav .bx.bx-menu') as HTMLElement;
+  const sidebar = document.getElementById('sidebar') as HTMLElement;
+
+  menuBar.addEventListener('click', function () {
+    sidebar.classList.toggle('hide');
+  })
+}
+
+const searchButton = () => {
+  const searchButton = document.querySelector('#content nav form .form-input button') as HTMLElement;
+  const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx') as HTMLElement;
+  const searchForm = document.querySelector('#content nav form') as HTMLElement;
+  const sidebar = document.getElementById('sidebar') as HTMLElement;
+
+  searchButton.addEventListener('click', function (e) {
+    if (window.innerWidth < 576) {
+      e.preventDefault();
+      searchForm.classList.toggle('show');
+      if (searchForm.classList.contains('show')) {
+        searchButtonIcon.classList.replace('bx-search', 'bx-x');
+      } else {
+        searchButtonIcon.classList.replace('bx-x', 'bx-search');
+      }
+    }
+  })
+
+  if (window.innerWidth < 768) {
+    sidebar.classList.add('hide');
+  } else if (window.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
+  }
+
+  window.addEventListener('resize', function () {
+    if (this.innerWidth > 576) {
+      searchButtonIcon.classList.replace('bx-x', 'bx-search');
+      searchForm.classList.remove('show');
+    }
+  })
+}
+
+const switchMode = () => {
+  const switchMode = document.getElementById('switch-mode') as HTMLInputElement;
+
+  switchMode.addEventListener('change', function () {
+    if (this.checked) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  })
+}
+
+onMounted(() => {
+
+  allSideMenu();
+
+  menuBar();
+
+  searchButton();
+
+  switchMode();
+})
+
+</script>
+
+<template>
+  <!-- <Sidebar />
+                      <Content /> -->
+
+  <!-- SIDEBAR -->
+  <section id="sidebar">
+    <a href="#" class="brand">
+      <i class='bx bxs-smile'></i>
+      <span class="text">AdminHub</span>
+    </a>
+    <ul class="side-menu top">
+      <li class="active">
+        <a href="#">
+          <i class='bx bxs-dashboard'></i>
+          <span class="text">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bxs-shopping-bag-alt'></i>
+          <span class="text">My Store</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bxs-doughnut-chart'></i>
+          <span class="text">Analytics</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bxs-message-dots'></i>
+          <span class="text">Message</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bxs-group'></i>
+          <span class="text">Team</span>
+        </a>
+      </li>
+    </ul>
+    <ul class="side-menu">
+      <li>
+        <a href="#">
+          <i class='bx bxs-cog'></i>
+          <span class="text">Settings</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="logout">
+          <i class='bx bxs-log-out-circle'></i>
+          <span class="text">Logout</span>
+        </a>
+      </li>
+    </ul>
+  </section>
+  <!-- SIDEBAR -->
+
+
+
+  <!-- CONTENT -->
+  <section id="content">
+    <!-- NAVBAR -->
+    <nav>
+      <i class='bx bx-menu'></i>
+      <a href="#" class="nav-link">Categories</a>
+      <form action="#">
+        <div class="form-input">
+          <input type="search" placeholder="Search...">
+          <button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
+        </div>
+      </form>
+      <input type="checkbox" id="switch-mode" hidden>
+      <label for="switch-mode" class="switch-mode"></label>
+      <a href="#" class="notification">
+        <i class='bx bxs-bell'></i>
+        <span class="num">8</span>
+      </a>
+      <a href="#" class="profile">
+        <img src="img/people.png">
+      </a>
+    </nav>
+    <!-- NAVBAR -->
+
+    <!-- MAIN -->
+    <main>
+      <div class="head-title">
+        <div class="left">
+          <h1>Dashboard</h1>
+          <ul class="breadcrumb">
+            <li>
+              <a href="#">Dashboard</a>
+            </li>
+            <li><i class='bx bx-chevron-right'></i></li>
+            <li>
+              <a class="active" href="#">Home</a>
+            </li>
+          </ul>
+        </div>
+        <a href="#" class="btn-download">
+          <i class='bx bxs-cloud-download'></i>
+          <span class="text">Download PDF</span>
+        </a>
+      </div>
+
+      <ul class="box-info">
+        <li>
+          <i class='bx bxs-calendar-check'></i>
+          <span class="text">
+            <h3>1020</h3>
+            <p>New Order</p>
+          </span>
+        </li>
+        <li>
+          <i class='bx bxs-group'></i>
+          <span class="text">
+            <h3>2834</h3>
+            <p>Visitors</p>
+          </span>
+        </li>
+        <li>
+          <i class='bx bxs-dollar-circle'></i>
+          <span class="text">
+            <h3>$2543</h3>
+            <p>Total Sales</p>
+          </span>
+        </li>
+      </ul>
+
+
+      <div class="table-data">
+        <div class="order">
+          <div class="head">
+            <h3>Recent Orders</h3>
+            <i class='bx bx-search'></i>
+            <i class='bx bx-filter'></i>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>User</th>
+                <th>Date Order</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <img src="img/people.png">
+                  <p>John Doe</p>
+                </td>
+                <td>01-10-2021</td>
+                <td><span class="status completed">Completed</span></td>
+              </tr>
+              <tr>
+                <td>
+                  <img src="img/people.png">
+                  <p>John Doe</p>
+                </td>
+                <td>01-10-2021</td>
+                <td><span class="status pending">Pending</span></td>
+              </tr>
+              <tr>
+                <td>
+                  <img src="img/people.png">
+                  <p>John Doe</p>
+                </td>
+                <td>01-10-2021</td>
+                <td><span class="status process">Process</span></td>
+              </tr>
+              <tr>
+                <td>
+                  <img src="img/people.png">
+                  <p>John Doe</p>
+                </td>
+                <td>01-10-2021</td>
+                <td><span class="status pending">Pending</span></td>
+              </tr>
+              <tr>
+                <td>
+                  <img src="img/people.png">
+                  <p>John Doe</p>
+                </td>
+                <td>01-10-2021</td>
+                <td><span class="status completed">Completed</span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="todo">
+          <div class="head">
+            <h3>Todos</h3>
+            <i class='bx bx-plus'></i>
+            <i class='bx bx-filter'></i>
+          </div>
+          <ul class="todo-list">
+            <li class="completed">
+              <p>Todo List</p>
+              <i class='bx bx-dots-vertical-rounded'></i>
+            </li>
+            <li class="completed">
+              <p>Todo List</p>
+              <i class='bx bx-dots-vertical-rounded'></i>
+            </li>
+            <li class="not-completed">
+              <p>Todo List</p>
+              <i class='bx bx-dots-vertical-rounded'></i>
+            </li>
+            <li class="completed">
+              <p>Todo List</p>
+              <i class='bx bx-dots-vertical-rounded'></i>
+            </li>
+            <li class="not-completed">
+              <p>Todo List</p>
+              <i class='bx bx-dots-vertical-rounded'></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+    <!-- MAIN -->
+  </section>
+  <!-- CONTENT -->
+</template>
+
+
+<style src="./style.css" />
---
+++ src/components/Content.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Navbar from "./Navbar.vue";
+import Main from "./Main.vue";
+</script>
+
+<template>
+    <section id="content">
+        <Navbar />
+        <Main />
+    </section>
+</template>
+
+<style scoped>
+/* #content {
+	position: relative;
+	width: calc(100% - 280px);
+	left: 280px;
+	transition: .3s ease;
+}
+#sidebar.hide ~ #content {
+	width: calc(100% - 60px);
+	left: 60px;
+} */
+</style>
