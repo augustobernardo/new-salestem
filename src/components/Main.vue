db645ac2452139e92dfa6cdd6fc81a5dac00531e
augustobernardo <gutooliveira027@gmail.com>
2023-04-14 10:17:33 -0300

    Alterando os ícones e adicionando o boxicons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,23 +5,27 @@ import Cards from './Cards.vue';
 
 <template>
     <main>
-        <div class="head-title">
-            <div class="left">
-                <h1>Dashboard</h1>
-                <ul class="breadcrumb">
+    <div class="head-title">
+        <div class="left">
+            <h1>Dashboard</h1>
+            <!-- <ul class="breadcrumb">
                     <li>
                         <a href="#">Dashboard</a>
                     </li>
-                    <li><i class='bx bx-chevron-right'></i></li>
                     <li>
-                        <a class="active" href="#">Home</a>
-                    </li>
-                </ul>
+                        <span class="material-icons-round">
+                            chevron_right
+                        </span>
+                        </li>
+                        <li>
+                    <a class="active" href="#">Home</a>
+                </li>
+            </ul> -->
             </div>
             <!-- <a href="#" class="btn-download">
-                <i class='bx bxs-cloud-download'></i>
-                <span class="text">Download PDF</span>
-            </a> -->
+                        <i class='bx bxs-cloud-download'></i>
+                        <span class="text">Download PDF</span>
+                    </a> -->
         </div>
 
         <Cards />
