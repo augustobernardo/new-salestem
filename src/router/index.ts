093133d1f9e2ef2c234ea16c454691ec1a8fdeae
Augusto Oliveira <gutooliveira027@gmail.com>
2023-06-07 09:25:35 -0300

    Ajustanto erro cometido no commit anterior

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,27 +4,27 @@ const routes = [
   {
     path: "/",
     name: "Login",
-    component: () => import("../views/LoginView.vue"),
+    component: () => import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
   },
   {
     path: "/dashboard",
     name: "Dashboard",
-    component: () => import(/* webpackChunkName: "dashboard" */ "../views/DashboardHomeView.vue"),
+    component: () => import(/* webpackChunkName: "dashboard" */ "../views/DashboardHomeView.vue.vue"),
   },
   {
     path: "/vendas",
     name: "Venda",
-    component: () => import(/* webpackChunkName: "vendas" */ "../views/VendasView.vue"),
+    component: () => import(/* webpackChunkName: "vendas" */ "../views/VendasView.vue.vue"),
   },
   {
     path: "/produtos",
     name: "Produto",
-    component: () => import(/* webpackChunkName: "produtos" */ "../views/ProdutosView.vue"),
+    component: () => import(/* webpackChunkName: "produtos" */ "../views/VendasView.vue"),
   },
   {
     path: "/clientes",
     name: "Cliente",
-    component: () => import(/* webpackChunkName: "clientes" */ "../views/ClientesView.vue"),
+    component: () => import(/* webpackChunkName: "clientes" */ "../views/ClientesView.vue.vue"),
   },
 ];
 
