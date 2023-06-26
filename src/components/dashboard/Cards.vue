c86408135f48d6ef81cda23708ae67bbe09005f5
augustobernardo <gutooliveira027@gmail.com>
2023-06-26 20:07:02 -0300

    Adicionando animações nos cards

--- src/components/dashboard/Cards.vue
+++ src/components/dashboard/Cards.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import { useCard } from "../../store/cardStore";
 import ChartComponent from "./ChartComponent.vue"
+import { onMounted } from "vue";
+import { gsap } from "gsap";
 
 const cardStore = useCard();
 const cardData = cardStore.cardData;
 
-// localStorage.setItem("cardData", JSON.stringify(cardData));
+onMounted(() => {
+    gsap.from(".box-info li", {
+        duration: 0.8,
+        y: 25,
+        opacity: 0,
+        stagger: 0.2,
+        ease: "ease-in-out",
+    });
+});
+
 </script>
 
 <template>
