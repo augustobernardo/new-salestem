8b9543a0f0ec483ea65b7caaa9ff8b620528c7a7
Enrico Gollner <enricogollner.tech@gmail.com>
2023-06-30 22:28:23 -0300

    Update

--- src/components/vendas/VendasMain.vue
+++ src/components/vendas/VendasMain.vue
@@ -4,7 +4,6 @@
       <div class="left">
         <h1>Vendas</h1>
       </div>
-
     </div>
 
     <div>
@@ -14,31 +13,33 @@
             <th>Nome do Cliente</th>
             <th>Nome do Produto</th>
             <th>Quantidade</th>
-            <th>Total Price</th>
+            <th>Total</th>
             <th>Data da Venda</th>
             <th>Edição</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(item, index) in items" :key="index">
+          <tr v-for="(item, index) in items" :key="index" :class="{ 'row-editing': item.editing }">
             <td>
-              <input v-model="item.client.name" :disabled="!item.editing" />
+              <input v-model="item.client.name" :disabled="!item.editing" class="input-field" />
             </td>
             <td>
-              <input v-model="item.products[0].nameProd" :disabled="!item.editing" />
+              <input v-model="item.product.nameProd" :disabled="!item.editing" class="input-field" />
             </td>
             <td>
-              <input v-model="item.products[0].amount" :disabled="!item.editing" />
+              <input v-model="item.amount" :disabled="!item.editing" class="input-field" />
             </td>
             <td>
-              <input v-model="item.totalPrice" :disabled="!item.editing" />
+              <input :value={resultTotal(index)}
+                :disabled="!item.editing" class="input-field" />
             </td>
             <td>
-              <input v-model="item.saleDate" :disabled="!item.editing" />
+              <input v-model="item.saleDate" :disabled="!item.editing" class="input-field" />
             </td>
             <td>
-              <button @click="editItem(item)">Editar</button>
-              <button @click="saveItem(item)" v-if="item.editing">Salvar</button>
+              <button @click="editItem(item)" v-if="!item.editing" class="edit-button">Editar</button>
+              <button @click="saveItem(item)" v-if="item.editing" class="save-button">Salvar</button>
+              <button @click="cancelEdit(item)" v-if="item.editing" class="cancel-button">Cancelar</button>
             </td>
           </tr>
         </tbody>
@@ -46,28 +47,58 @@
     </div>
 
     <div>
-      <button @click="showModal = true">Registrar Nova Venda</button>
+      <button @click="showModal = true" class="add-button">Registrar Nova Venda</button>
       <div v-if="showModal" class="modal">
-        <!-- Modal content for registering new sale -->
+        <div class="modal-content">
+          <h2>Registrar Nova Venda</h2>
+          <form @submit.prevent="registrarVenda">
+            <div class="form-group">
+              <label for="cliente">Cliente:</label>
+              <select id="cliente" v-model="novoSale.client" required class="input-field">
+                <option v-for="client in clients" :key="client.codClient" :value="client">{{ client.name }}</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="totalPrice">Total Price:</label>
+              <input id="totalPrice" v-model="novoSale.totalPrice" type="number" required class="input-field" />
+            </div>
+            <div class="form-group">
+              <label for="saleDate">Data da Venda:</label>
+              <input id="saleDate" v-model="novoSale.saleDate" type="date" required class="input-field" />
+            </div>
+            <div class="button-group">
+              <button type="submit" class="add-row-button">Registrar</button>
+              <button @click="fecharModal" class="cancel-button">Fechar</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </main>
 </template>
-  
+
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
 import SaleModel from "../../models/saleModel";
 import SalesController from "../../controllers/salesController";
+import ClientModel from "../../models/clientModel";
+import ProductModel from "../../models/productModel";
 
 interface Item {
   client: { name: string };
-  products: { nameProd: string; amount: number }[];
+  product: { nameProd: string; amount: number };
   totalPrice: number;
   saleDate: string;
   editing: boolean;
 }
 
+
+
 const salesController = new SalesController();
+
+const resultTotal = (index: number) => {
+  return index
+}
 
 export default defineComponent({
   name: "DataTable",
@@ -76,15 +107,24 @@ export default defineComponent({
     const showModal = ref(false);
     const novoSale = ref<SaleModel>({
       codSale: 0,
+      amount: 0,
       totalPrice: 0,
       saleDate: "",
       client: { codClient: 0, name: "", phone: "", email: "", cpf: "" },
-      products: [{ codProd: 0, nameProd: "", price: 0, stock: 0, amount: 0 }],
+      product: { codProd: 0, nameProd: "", price: 0, stock: 0, amount: 0 },
     });
+    const clients = ref<ClientModel[]>([]); // Add clients data
+    const products = ref<ProductModel[]>([]); // Add products data
 
     onMounted(() => {
       reloadTabela();
     });
+
+    let total = 0
+
+    function getTotalPrice(amount: number, price: number) {
+      total = amount * price
+    }
 
     function reloadTabela() {
       salesController
@@ -92,6 +132,7 @@ export default defineComponent({
         .then((sales) => sales)
         .then((data) => {
           items.value = data;
+          console.log(data)
         })
         .catch((error) => {
           console.error("Erro ao obter os dados da API:", error);
@@ -99,16 +140,17 @@ export default defineComponent({
     }
 
     function editItem(item: Item) {
-      if (item.editing) {
-        item.editing = false;
-      } else {
-        item.editing = true;
-      }
+      item.editing = true;
     }
 
     function saveItem(item: Item) {
       item.editing = false;
       salesController.updateSale(item);
+      reloadTabela();
+    }
+
+    function cancelEdit(item: Item) {
+      item.editing = false;
       reloadTabela();
     }
 
@@ -129,16 +171,16 @@ export default defineComponent({
       novoSale,
       editItem,
       saveItem,
+      cancelEdit,
       registrarVenda,
       fecharModal,
+      clients,
+      products,
+      getTotalPrice
     };
   },
 });
 </script>
-  
-
-
-
 
 
 <style scoped>
@@ -228,6 +270,29 @@ td {
 
 .left {
   margin: 10px;
+}
+
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 4px;
+  width: 400px;
+}
+
+h2 {
+  margin-bottom: 20px;
 }
 </style>
   
